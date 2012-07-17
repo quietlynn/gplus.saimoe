5a25149c6e139160beab9b637167d2f0fd4dc0d7
Jingqin Lynn <nua8ytscerou8emworki6viyubl0gafa@quietmusic.org>
2012-07-17 23:40:26 +0800

    Fixed the order of bootstrap responsive css.

--- Saimoe/Global.asax.cs
+++ Saimoe/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Microsoft.Web.Optimization;
 
 namespace Saimoe
 {
@@ -19,7 +20,14 @@ namespace Saimoe
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
 
-            Microsoft.Web.Optimization.BundleTable.Bundles.EnableDefaultBundles();
+            BundleTable.Bundles.EnableDefaultBundles();
+            var bootstrapOrdering = new BundleFileSetOrdering("bootstrap");
+
+            // Responsive plugin should be loaded later than the main css.
+            bootstrapOrdering.Files.Add("bootstrap.css");
+            bootstrapOrdering.Files.Add("bootstrap-responsive.css");
+
+            BundleTable.Bundles.FileSetOrderList.Add(bootstrapOrdering);
         }
     }
 }
