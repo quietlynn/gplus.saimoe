1dd6a98f2c5529c5a34ddfa90d37bc1f0cb564cd
Jingqin Lynn <nua8ytscerou8emworki6viyubl0gafa@quietmusic.org>
2012-07-17 19:09:31 +0800

    Add some packages. Create basic Views directory struct.

---
+++ Saimoe/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Saimoe.Controllers
+{
+    public class TestController : Controller
+    {
+        ActionResult result = new EmptyResult();
+
+        [System.Diagnostics.Conditional("DEBUG")]
+        private void handleRequest(string viewName)
+        {
+            result = View(viewName);
+        }
+
+        //
+        // GET: /Test/View/{id}
+        [ActionName("View")]
+        public ActionResult ViewAction(string id)
+        {
+            handleRequest(id);
+            return result;
+        }
+
+    }
+}
