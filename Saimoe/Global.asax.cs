0da4fd14c12e97b69c15f8c7148c56c0153a2e0c
Jingqin Lynn <nua8ytscerou8emworki6viyubl0gafa@quietmusic.org>
2012-07-17 20:47:22 +0800

    Fix the default bundles.

--- Saimoe/Global.asax.cs
+++ Saimoe/Global.asax.cs
@@ -18,6 +18,8 @@ namespace Saimoe
 
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            Microsoft.Web.Optimization.BundleTable.Bundles.EnableDefaultBundles();
         }
     }
 }
