5f043f00d1c2c31700212a3bc277c3c6d9bd5de0
Jingqin Lynn <nua8ytscerou8emworki6viyubl0gafa@quietmusic.org>
2012-07-18 16:20:01 +0800

    Add resources for i18n.

---
+++ Saimoe/HtmlHelperExtensions/ModelMetadataExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using System.Linq.Expressions;
+
+namespace Saimoe.HtmlHelperExtensions
+{
+    public static class ModelMetadataExtensions
+    {
+        public static MvcHtmlString DescriptionFor<TModel, TValue>(this HtmlHelper<TModel> html, Expression<Func<TModel, TValue>> expression)
+        {
+            var tagBuilder = new TagBuilder("div");
+            tagBuilder.SetInnerText(ModelMetadata.FromLambdaExpression<TModel, TValue>(expression, html.ViewData).Description);
+            tagBuilder.MergeAttribute("data-description-for", ExpressionHelper.GetExpressionText(expression));
+            tagBuilder.AddCssClass("description");
+            return new MvcHtmlString(tagBuilder.ToString());
+        }
+    }
+}
