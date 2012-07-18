d0e9242fd7b35a475b739540d7d2a47e0eb67b79
Jingqin Lynn <nua8ytscerou8emworki6viyubl0gafa@quietmusic.org>
2012-07-18 16:59:11 +0800

    Add a ContestRegistration model.

---
+++ Saimoe/Models/ContestantRegistration.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Saimoe.Models
+{
+    public class ContestantRegistration
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(
+            Name = "Tagline",
+            Description = "TaglineDescription",
+            ResourceType = typeof(Resources.WebResources)
+        )]
+        public string Tagline { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(
+            Name = "Interest",
+            Description = "InterestDescription",
+            ResourceType = typeof(Resources.WebResources)
+        )]
+        public string Interest { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(
+            Name = "Characteristic",
+            Description = "CharacteristicDescription",
+            ResourceType = typeof(Resources.WebResources)
+        )]
+        public string Characteristic { get; set; }
+
+        [Required]
+        [DataType(DataType.Url)]
+        [Display(
+            Name = "RegistrationPost",
+            Description = "RegistrationPostDescription",
+            ResourceType = typeof(Resources.WebResources)
+        )]
+        public string RegistrationPost { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(
+            Name = "ActingCute",
+            Description = "ActingCuteDescription",
+            ResourceType = typeof(Resources.WebResources)
+        )]
+        public string ActingCute { get; set; }
+    }
+}
