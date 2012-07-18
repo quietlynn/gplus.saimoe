20e314e8146be8066e6a5c74c2ef4751fe076218
Tsinghua <mytsingh@gmail.com>
2012-07-18 22:59:05 +0800

    Add contestant adding functionality

---
+++ Saimoe/Core/ContestantService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Saimoe.Models;
+using Saimoe.Models.EF;
+using Saimoe.Repository;
+
+namespace Saimoe.Core
+{
+    public class ContestantService
+    {
+
+        public void AddContestant(string googlePlusId, ContestantRegistration registrationInfo)
+        {
+            if (string.IsNullOrEmpty(googlePlusId))
+            {
+                throw new ArgumentNullException("GPlus Id");
+            }
+
+            if (registrationInfo == null)
+            {
+                throw new ArgumentNullException("Contestant Registration");
+            }
+
+            var repository = new ContestantRepository();
+            var contestant = new Contestant
+            {
+                GooglePlusId = googlePlusId,
+                CreatedDate = DateTime.Now,
+                LastLoginDate = DateTime.Now,
+                Profile = AutoMapper.Mapper.Map<ContestantRegistration, Profile>(registrationInfo)
+            };
+            repository.AddContestant(contestant);
+        }
+
+        /// <summary>
+        /// Get contestant info by given google plus id
+        /// </summary>
+        /// <param name="gPlusId">google plus id</param>
+        /// <returns>found contestant info or null</returns>
+        public Contestant GetContestant(string googlePlusId)
+        {
+            if (string.IsNullOrEmpty(googlePlusId))
+            {
+                throw new ArgumentNullException("GPlus Id");
+            }
+
+            var repository = new ContestantRepository();
+            return repository.GetContestant(googlePlusId);
+        }
+    }
+}
---
+++ Saimoe/Repository/ContestantRepository.cs
@@ -0,0 +1,56 @@
+﻿/**
+ * @file: Repository/ContestantRepository.cs
+ * @author: Tsinghua <mytsingh@gmail.com>.
+ * @description:
+ * The contestant repository for CRUD contestants
+ * 
+ */
+using System.Linq;
+using Saimoe.Models.EF;
+
+namespace Saimoe.Repository
+{
+    public class ContestantRepository
+    {
+        SaimoeContext _dbContext;
+
+        #region Constructor
+        /// <summary>
+        /// Initialize a repository with given connection string.
+        /// </summary>
+        /// <param name="connectionString">connection string</param>
+        public ContestantRepository(string connectionString)
+        {
+            _dbContext = new SaimoeContext(connectionString);
+        }
+
+        /// <summary>
+        /// Initialize a repository with default connection string found in the 'SaimoeContext' section of the application configuration file.
+        /// </summary>
+        public ContestantRepository()
+        {
+            _dbContext = new SaimoeContext();
+        }
+        #endregion
+
+        /// <summary>
+        /// Add a new contestant
+        /// </summary>
+        /// <param name="contestant">the new contestant</param>
+        internal void AddContestant(Contestant contestant)
+        {
+            _dbContext.Contestants.AddObject(contestant);
+            _dbContext.SaveChanges();
+        }
+
+        /// <summary>
+        /// Get contestant info by google plus id
+        /// </summary>
+        /// <param name="googlePlusId">google plus id</param>
+        /// <returns>found contestant or null</returns>
+        internal Contestant GetContestant(string googlePlusId)
+        {
+            return _dbContext.Contestants.SingleOrDefault(c => c.GooglePlusId == googlePlusId);
+        }
+    }
+}
